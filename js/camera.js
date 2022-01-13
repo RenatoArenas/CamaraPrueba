cd060f50dbd94ca16695cbe1172fecf975e63194
RenatoArenas <renato.arenas2012@gmail.com>
2022-01-13 12:32:08 -0500

    Añadidos estilos a botón de agregar imagenes

--- js/camera.js
+++ js/camera.js
@@ -1,20 +1,7 @@
-function validarExt() {
-    var imgInput = document.getElementById('imgInput');
-    var imgRuta = imgInput.value;
-    var extPermitidas = /(.jpg|.PNG|.svg|.webp)$/i;
-    console.log(extPermitidas)
-    
-    if (!extPermitidas.exec(imgRuta)) {
-        alert('Solo se permiten imagenes');
-        imgInput.value = "";
-    }
-    else {
-        if(imgInput.files && imgInput.files[0]) {
-            var visor = new FileReader();
-            visor.onload=function(e) {
-                document.getElementById('visorImg').innerHTML = '<embed src="'+e.target.result+'" width="500" height="500" >';
-            }
-            visor.readAsDataURL(imgInput.files[0]);
-        }
-    }
-}
+$(document).ready(function() {
+
+    $(document).on("click", "#add-new-photo", function() {
+        $("#imgInput").click();
+    });
+
+});
