f0fde6053ada8ad6af6dc722b01fa28f95df34c8
RenatoArenas <renato.arenas2012@gmail.com>
2022-01-13 17:10:28 -0500

    Agregado código para visualizar y agregar imagenes

--- js/camera.js
+++ js/camera.js
@@ -4,4 +4,22 @@ $(document).ready(function() {
         $("#imgInput").click();
     });
 
+    $(document).on("change", "#imgInput", function() {
+        console.log(this.files);
+        let files = this.files;
+        let element;
+        let validatedImages = ["image/png", "image/jpeg", "image/svg"];
+        let elementsNoValids = false;
+
+        for (let i = 0; i < files.length; i++) {
+            element = files[i];
+
+            if (validatedImages.indexOf(element.type) != -1) {
+                createPreview(element);
+            }
+            else {
+                elementsNoValids = true;
+            }
+        }
+    });
 });
